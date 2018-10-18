6e4920f85ec646de77c3a73ca355f12849ae2df2
proman24 <mathur.geetanshu887@gmail.com>
2018-10-18 07:52:52 +0530

    added year field

--- script.js
+++ script.js
@@ -20,10 +20,12 @@ function add_elements(){
         {
             "name":"Geetanshu Mathur",
             "branch":"Computer Science and Engineering (VII Semester)",
+            "year" : "2015"
         },
         {
             "name":"Sharthak Vijayvergiya",
             "branch":"Computer Science and Engineering (VII Semester)",
+            "year":"2015"
         },
     ]
     par = document.getElementById("participants")
@@ -39,8 +41,13 @@ function add_elements(){
         branch.className = "branch";
         branch.appendChild(document.createTextNode(element.branch));
 
+        var year = document.createElement("div");
+        year.className = "year";
+        year.appendChild(document.createTextNode(element.year));
+
         student.appendChild(name);
         student.appendChild(branch);
+        student.appendChild(year);
 
         par.appendChild(student);
     });
